71b4ac2d0529cd8e9683111c61789213931425d0
Isabel_Kikuvi <isabel.kikuvi@gmail.com>
2023-05-18 03:47:48 -0700

    updates

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,6 +23,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		temp = temp->next;
 		count++;
+
+		if (temp == NULL)
+			return (NULL);
 	}
 
 	if (temp->next == NULL)
